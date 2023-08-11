2337e42ef316530e3bf115089a4285c5d515e9bd
Gorka Echavarri <gorkaechavarri@gmail.com>
2023-08-11 12:05:34 +0100

    working components linked

---
+++ src/components/NewGrocery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NewGrocery() {
+  return (
+    <div>
+      <p>Form to create new Grocery for the list</p>
+    </div>
+  );
+}
+
+export default NewGrocery;
