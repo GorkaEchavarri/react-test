2bb62bdbbf23b6ad2c1f8562d7a83e4a78a58066
Gorka Echavarri <gorkaechavarri@gmail.com>
2023-08-21 13:27:07 +0100

    filter done

--- src/App.js
+++ src/App.js
@@ -10,13 +10,31 @@ const first_groceries = [
   {key: 456, type: "Cleaning", quantity: 1, item: "Bleach"},
   {key: 136,type: "Food", quantity: 2, item: "Cookies"},
   {key: 412,type: "Bathroom", quantity: 2, item: "Toilet Paper"},
-  {key: 832,type: "Bathroom", quantity: 2, item: "Hair Wash"},
+  {key: 832,type: "Food", quantity: 2, item: "Oreos"},
   {key: 375,type: "Bathroom", quantity: 2, item: "Toothbrush"},
 ]
 
 function App() {
 
   const [groceries, setGroceries] = useState(first_groceries);
+  const [filteredType, setFilteredType] = useState("All");
+
+  let showedInfo = "All";
+
+  if (filteredType === "food") {
+    showedInfo = "Food"
+  } else if (filteredType === "cleaning") {
+    showedInfo = "Cleaning"
+  } else if (filteredType === "kitchen") {
+    showedInfo = "Kitchen"
+  } else if (filteredType === "bathroom") {
+    showedInfo = "Bathroom"
+  } else {
+    showedInfo = "All"
+  }
+
+  const filtered_groceries = first_groceries.filter(expense => expense.type === filteredType.charAt(0).toUpperCase() + filteredType.slice(1))
+  console.log(filtered_groceries)
 
   function addGroceryHandler(item) {
     setGroceries((prevGroceries) => {
@@ -24,12 +42,19 @@ function App() {
     });
   };
 
+  function filterHandler(selectedType) {
+    setFilteredType(selectedType);
+  };
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>--My Groceries--</h1>
         <NewGrocery onSaveGroceryData={addGroceryHandler} />
-        <GroceryFilter />
+        <GroceryFilter selected={filteredType} onFilterChange={filterHandler} />
+        <p>Data for the type {showedInfo} is shown.</p>
         <ListAllGroceries items={groceries}/>
       </header>
     </div>
--- src/components/GroceryFilter.js
+++ src/components/GroceryFilter.js
@@ -1,11 +1,16 @@
 import './GroceryFilter.css'
 
 function GroceryItem(props) {
+
+  function filterHandler(event) {
+    props.onFilterChange(event.target.value)
+  };
+
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Choose a type to filter: </label>
-        <select >
+        <select value={props.selected} onChange={filterHandler}>
           <option value='all'>All</option>
           <option value='food'>Food</option>
           <option value='cleaning'>Cleaning</option>
